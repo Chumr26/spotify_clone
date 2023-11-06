490f003e9bde5c041c2504305e3a9dd098f4e394
Chumr26 <nguyenanhkhoa26092002@gmail.com>
2023-11-06 18:12:07 +0700

    Songs fetching and list display

--- components/Library.tsx
+++ components/Library.tsx
@@ -7,7 +7,15 @@ import useAuthModal from '@/hooks/useAuthModal';
 import { useUser } from '@/hooks/useUser';
 import useUploadModal from '@/hooks/useUploadModal';
 
-export default function Library() {
+import { Song } from '@/types';
+
+import MediaItem from './MediaItem';
+
+interface LibraryProps {
+    songs: Song[];
+}
+
+export default function Library({ songs }: LibraryProps) {
     const authModal = useAuthModal();
     const uploadModal = useUploadModal();
     const { user } = useUser();
@@ -34,7 +42,9 @@ export default function Library() {
                 />
             </div>
             <div className="flex flex-col gap-y-2 mt-4 px-3">
-                List of songs...
+                {songs.map((song) => (
+                    <MediaItem onClick={() => {}} key={song.id} data={song} />
+                ))}
             </div>
         </div>
     );
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,11 +10,14 @@ import Box from './Box';
 import SidebarItem from './SidebarItem';
 import Library from './Library';
 
+import { Song } from '@/types';
+
 interface SidebarProps {
     children: React.ReactNode;
+    songs: Song[];
 }
 
-export default function Sidebar({ children }: SidebarProps) {
+export default function Sidebar({ children, songs }: SidebarProps) {
     const pathname = usePathname();
     const routes = useMemo(
         () => [
@@ -44,7 +47,7 @@ export default function Sidebar({ children }: SidebarProps) {
                     </div>
                 </Box>
                 <Box className="overflow-auto h-full">
-                    <Library />
+                    <Library songs={songs} />
                 </Box>
             </div>
             <main className="h-full flex-1 overflow-y-auto py-2">
